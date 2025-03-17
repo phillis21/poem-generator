fa8340ae1f7fabbb4a4472d9e720e65ebc1f349d
phillis21 <philliswadin@gmail.com>
2025-03-17 05:21:36 +0200

    added basic javaScript and used typewrite plugin to enter a poem

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-alert("Welcome");
+function generatePoem(event){
+    event.preventDefault();
+
+    alert("Generating a poem");
+
+    new Typewriter("#poem", {
+        strings: "Sun of my Soul, oh Savior dear",
+        autoStart: true,
+        delay: 'natural',
+        cursor: "",
+    });
+}
+
+
+let poemForm = document.querySelector("#poem-gen-form");
+poemForm.addEventListener("submit",generatePoem);
